b7fb5bef41c48828d3b7b2a6a0a839e2cad3f88d
Fabrizio Gonzalez Serrati <fabrigs@hotmail.es>
2024-10-03 09:42:55 -0400

    Google Analytics
    Added Google Analytics support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
+
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-6SCBT5XYPE");
 export default function Home() {
   return (
     <div>
