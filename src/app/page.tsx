51e92476dc96a934f410981299d3c2cce60c9446
Fabrizio Gonzalez Serrati <fabrigs@hotmail.es>
2024-10-03 09:56:50 -0400

    Googla tags Added
    google tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
+"use client";
+import { useEffect } from "react";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
-
 import ReactGA from "react-ga4";
 
+// Inicializar Google Analytics
 ReactGA.initialize("G-6SCBT5XYPE");
+
 export default function Home() {
+  useEffect(() => {
+    // Google Tag Manager
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-6SCBT5XYPE";
+    script1.async = true;
+    document.head.appendChild(script1);
+
+    // gtag config
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-6SCBT5XYPE');
+    `;
+    document.head.appendChild(script2);
+
+    // Cleanup
+    return () => {
+      document.head.removeChild(script1);
+      document.head.removeChild(script2);
+    };
+  }, []);
+
   return (
     <div>
       <Header />
